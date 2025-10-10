ee501839b2ed35289f85b513f5937997ac9eae10
Darren Wong <58476988+darrenW04@users.noreply.github.com>
2025-10-10 16:57:43 -0400

    database restructure
    Added dummy data endpoints

--- backend/api_models.py
+++ backend/api_models.py
@@ -1,11 +1,50 @@
 from pydantic import BaseModel
+from typing import Optional
 
-class UserPydantic(BaseModel):
-    """Data model for creating a user."""
-    username: str
-    password: str
 
 class SessionPydantic(BaseModel):
-    """Data model for logging in."""
-    username: str
+    email: str
     password: str
+
+class SessionDeletePydantic(BaseModel):
+    sessionID: str
+
+class CourseDeletePydantic(BaseModel):
+    name: str
+    cid: Optional[str] = None
+    semester: str
+
+class updateUser(BaseModel):
+    name:str
+    sessionID:str
+    email:str
+    phone:str
+    newPassword:str
+    major:str
+    degree:str
+
+class UserPydantic(BaseModel):
+     name: str
+     email: str
+     phone: str
+     password: str
+     major: str
+     degree: str
+
+class UserDeletePydantic(BaseModel):
+    sessionID: str
+    password: str
+
+class UserCoursePydantic(BaseModel):
+    name: str
+    semester: str
+    cid: str
+
+class SubsemesterPydantic(BaseModel):
+    semester: Optional[str] = None
+
+class DefaultSemesterSetPydantic(BaseModel):
+    default: str
+
+
+    
--- backend/controllers/user_controller.py
+++ backend/controllers/user_controller.py
@@ -9,3 +9,54 @@ def delete_current_user(user_id: int):
     print(f"DATABASE: Deleting user with ID {user_id}...")
     # In a real app, you would delete the user from the database here.
     return {"status": "success", "message": "User deleted."}
+
+# def add_user(form):
+#     users = UserModel()
+
+#     if not assert_keys_in_form_exist(form, ['name', 'email', 'phone', 'password', 'major', 'degree']):
+#         return msg.error_msg("Please check your requests.")
+
+#     name = form['name']
+#     email = form['email']
+#     phone = form['phone']
+#     password = form['password']
+#     major = form['major']
+#     degree = form['degree']
+
+#     if name.strip() == "":
+#         return msg.error_msg("Username cannot be empty.")
+
+#     if password.strip() == "":
+#         return msg.error_msg("Password cannot be empty.")
+
+#     if len(password) < 6:
+#         return msg.error_msg("Password cannot less than 6 character.")
+
+#     if len(name) > 255:
+#         return msg.error_msg("Username cannot exceed 255 characters.")
+
+#     if len(password) > 255:
+#         return msg.error_msg("Password cannot exceed 255 characters.")
+
+#     findUser = users.get_user(email=email, enable=True)
+
+#     if findUser is None:
+#         return msg.error_msg("Failed to find user.")
+
+#     if len(findUser) != 0:
+#         return msg.error_msg("User already exists. (Email already in use)")
+
+#     args = {
+#         "Name": name,
+#         "Email": email,
+#         "Phone": phone,
+#         "Password": encrypt(password),
+#         "Major": major,
+#         "Degree": degree,
+#         "Enable": True
+#     }
+#     res = users.add_user(args)
+#     if res is None:
+#         return msg.error_msg("Failed to add user.")
+
+#     return msg.success_msg({"msg": "User added successfully."})
